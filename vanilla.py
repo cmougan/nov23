9055c25503bb89ffa446e2f0847d86bb37e7c9c0
Carlos Mougan <carmougan@gmail.com>
2023-11-23 18:19:03 +0100

    dummy0

--- vanilla.py
+++ vanilla.py
@@ -1,13 +1,25 @@
 # %%
 import pandas as pd
 from helper.helper import metric
+from sklearn.model_selection import train_test_split
 
 # %%
 # Read parquet file
 train_data = pd.read_parquet("data/train_data.parquet")
 
 # %%
-train_data["prediction"] = 0
+# Train Test Split
+y = train_data["phase"]
+X = train_data.drop(["phase"], axis=1)
+X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+# %%
+# Dummy Regressor
+from sklearn.dummy import DummyRegressor
+
+dummy = DummyRegressor(strategy="mean")
+dummy.fit(X, y)
+
+train_data["prediction"] = dummy.predict(X)
 # %%
 # Check performance
 print("Performance:", metric(train_data))
