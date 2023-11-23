45aafe196cb1cb812926dd87c743dca6d5006da4
Carlos Mougan <carmougan@gmail.com>
2023-11-23 18:31:25 +0100

    dummy1

--- vanilla.py
+++ vanilla.py
@@ -6,20 +6,23 @@ from sklearn.model_selection import train_test_split
 # %%
 # Read parquet file
 train_data = pd.read_parquet("data/train_data.parquet")
-
+# %%
+## Transform Phase based on monthly sales
+train_data["phase"] = train_data["phase"] * train_data["monthly"]
 # %%
 # Train Test Split
 y = train_data["phase"]
 X = train_data.drop(["phase"], axis=1)
 X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # %%
 # Dummy Regressor
 from sklearn.dummy import DummyRegressor
 
 dummy = DummyRegressor(strategy="mean")
-dummy.fit(X, y)
+dummy.fit(X_tr, y_tr)
 
-train_data["prediction"] = dummy.predict(X)
+train_data["prediction"] = dummy.predict(X) / train_data["monthly"]
 # %%
 # Check performance
 print("Performance:", metric(train_data))
