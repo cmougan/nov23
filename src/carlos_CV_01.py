fe9680383158e924558670291182ed3800b9b045
Carlos Mougan <carmougan@gmail.com>
2023-11-24 18:28:49 +0100

    CV function

---
+++ src/carlos_CV_01.py
@@ -0,0 +1,89 @@
+# %%
+import pandas as pd
+from helper.helper import metric, add_date_cols, scale_prediction, check_assert_sum_1
+from utils.validation import train_test_split_temporal
+
+# %%
+# Read files
+train_data = pd.read_parquet("data/train_data.parquet")
+## Transform phase based on monthly sales
+train_data["phase"] = train_data["phase"] * train_data["monthly"]
+
+# Load submission data
+submission_data = pd.read_parquet("data/submission_data.parquet")
+## Merge train and submission data
+# TODO: Discuss if we want to follow this engineering approach
+train_data["train"] = 1
+submission_data["train"] = 0
+df = pd.concat([train_data, submission_data], axis=0)
+# %%
+# Feature Engineering
+## TODO: Add feature engineering
+
+# Add date colummns
+df = add_date_cols(df)
+
+# %%
+# Train Test Split
+X_subm = df[df.train == 0]
+X = df[df.train == 1].drop(["phase"], axis=1)
+y = df[df.train == 1].phase
+# Temporal split
+X_tr, X_te, y_tr, y_te = train_test_split_temporal(
+    X, y, date_col="date", date_split="2021-01-01"
+)
+
+# %%
+# Model
+from sklearn.dummy import DummyRegressor
+
+model = DummyRegressor(strategy="mean")
+model.fit(X_tr, y_tr)
+
+
+# %%
+# Check performance
+## Train
+X_tr["prediction"] = model.predict(X_tr)
+X_tr = scale_prediction(X_tr)
+X_tr["phase"] = y_tr
+print("Train Performance:", metric(X_tr))
+
+## Test
+X_te["prediction"] = model.predict(X_te)
+X_te = scale_prediction(X_te)
+X_te["phase"] = y_te
+print("Test Performance:", metric(X_te))
+# %%
+# Assert phase sums 1 for each year-month-brand-country
+check_assert_sum_1(X_tr)
+check_assert_sum_1(X_te)
+# %%
+## PREPARE SUBMISSION
+# %%
+# Train in full pipeline
+model.fit(X, y)
+# Scale predictions
+X_subm["prediction"] = model.predict(X_subm)
+X_subm = scale_prediction(X_subm)
+
+# %%
+# Prepare file
+submission = pd.read_csv("data/submission_template.csv").drop(["prediction"], axis=1)
+submission = add_date_cols(submission)  ## TODO does this work?
+# %%
+# Merge submission template with predictions on "country", "brand", "date"
+submission = submission.merge(
+    X_subm[["country", "brand", "date", "prediction"]], on=["country", "brand", "date"]
+)
+submission = submission[["country", "brand", "date", "prediction"]]
+# %%
+# Check if submission has missing values
+print(submission.shape)
+submission.isna().sum()
+
+# %%
+# Save Submission
+sub_number = 3
+sub_name = "submissions/submission{}.csv".format(sub_number)
+submission.to_csv(sub_name, index=False)
--- src/distShifAnalysis.py
+++ src/distShifAnalysis.py
@@ -4,6 +4,7 @@ from src.helper.helper import (
     metric,
     scale_prediction,
     check_assert_sum_1,
+    add_date_cols,
 )
 from sklearn.model_selection import train_test_split
 from scipy.stats import ks_2samp
@@ -13,6 +14,8 @@ import matplotlib.pyplot as plt
 # %%
 # Read files
 train_data = pd.read_parquet("data/train_data.parquet")
+# date
+train_data = add_date_cols(train_data)
 ## Transform phase based on monthly sales
 train_data["phase"] = train_data["phase"] * train_data["monthly"]
 
@@ -26,7 +29,12 @@ for col in train_data_numeric.columns:
         pval = ks_2samp(train_data_numeric[col], submission_data[col]).pvalue
         plt.figure()
         plt.title(col + " pval: " + str(pval))
-        sns.kdeplot(train_data[col], label="train")
+        for year in train_data.year.unique():
+            sns.kdeplot(
+                train_data[train_data.year == year][col],
+                label="train " + str(year),
+            )
+
         sns.kdeplot(submission_data[col], label="test")
         plt.legend()
         plt.show()
