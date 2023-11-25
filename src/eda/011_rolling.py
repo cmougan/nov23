df1f8705d7b1ebd54498b53d77120c3e4a416f9e
David <david26694@gmail.com>
2023-11-25 07:55:07 +0100

    first rolling version

---
+++ src/eda/011_rolling.py
@@ -0,0 +1,27 @@
+# %% 
+import pandas as pd
+
+from src.utils.rolling import rolling_fn
+
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.max_rows", 300)
+# %%
+
+train_data = pd.read_parquet("data/train_data.parquet")
+submission_data = pd.read_parquet("data/submission_data.parquet")
+# %%
+all_data = pd.concat([train_data, submission_data]).sort_values(["brand", "country", "date"])
+# %%
+sample_df = all_data.query("brand == 'ZVLFE' and country == 'Zamunda'")
+sample_df
+
+# %%
+sample_df["rolling"] = rolling_fn(sample_df, groupby_cols=["brand", "country", "month", "wd"], column="phase", function="mean", rolling_periods=2, shift_periods=1)
+sample_df["rolling"]
+# %%
+sample_df.head(300)
+# %%
+sample_df.query("month == 1 and wd == 3")
+# %%
+all_data["rolling"] = rolling_fn(all_data, groupby_cols=["brand", "country", "month", "wd"], column="phase", function="mean", rolling_periods=2, shift_periods=1)
+# %%
