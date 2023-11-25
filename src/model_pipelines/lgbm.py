7289bbee83ad0144e8af344166bd160c00622c2a
David <david26694@gmail.com>
2023-11-25 07:22:07 +0100

    Structure pipeline

---
+++ src/model_pipelines/lgbm.py
@@ -0,0 +1,22 @@
+from category_encoders import OrdinalEncoder, TargetEncoder
+from lightgbm import LGBMRegressor
+from sklearn.pipeline import Pipeline
+
+
+class LGBMModelPipeline:
+    model_name: str = "lgbm"
+    def get_pipeline(self):
+        return Pipeline(
+            [
+                ("encoder", TargetEncoder(cols=["brand", "country", "main_channel", "ther_area"])),
+                ("model", LGBMRegressor(random_state=42, n_jobs=-1, verbose=0)),
+            ]
+        )
+
+    def get_grid(self):
+        return {
+            "model__n_estimators": [50, 100, 200],
+        }
+
+    def get_fit_kwargs(self, X_train):
+        return {"model__sample_weight": X_train["monthly"]}
