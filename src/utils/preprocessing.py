a444a160f1dd0585e391b18b9fb729b6d965fb5c
Leonardo Gonzalez <leonardo.gonzales@glovoapp.com>
2023-11-25 15:46:01 +0100

    Add covid_exogenous variable

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -5,7 +5,7 @@ import pandas as pd
 from typing import List
 
 
-def add_date_cols(df):
+def add_date_cols(df, add_weights=True):
     """
     Convert date to datetime and add year, month, quarter and week columns
     """
@@ -28,7 +28,8 @@ def add_date_cols(df):
         1,
         np.where(df["quarter"] == 2, 0.75, np.where(df["quarter"] == 3, 0.66, 0.5)),
     )
-    df["quarter_wm"] = df["quarter_w"] * df["monthly"]
+    if add_weights:
+         df["quarter_wm"] = df["quarter_w"] * df["monthly"]
 
     return df
 
@@ -98,6 +99,25 @@ def add_basic_lag_features(df, n_lags_day, n_lags_month, n_lags_yr):
     return df.reset_index()
 
 
+def add_basic_lag_features_week(df, n_lags_day, n_lags_month, n_lags_week):
+    df["date"] = pd.to_datetime(df["date"])
+    df = df.set_index("date")
+    for i in range(1, n_lags_day + 1):
+        df[f"lag_phase_{i}_days"] = df.phase.shift(i, freq="D")
+
+    for i in range(1, n_lags_month + 1):
+        df[f"lag_phase_{i}_month_exact"] = df.phase.shift(30 * i, freq="D")
+        df[f"lag_phase_{i}_month_before"] = df.phase.shift(30 * i - 1, freq="D")
+        df[f"lag_phase_{i}_month_after"] = df.phase.shift(30 * i + 1, freq="D")
+
+    for i in range(1, n_lags_week + 1):
+        df[f"lag_phase_{i}_week_exact"] = df.phase.shift(7 * i, freq="D")
+        df[f"lag_phase_{i}_week_before"] = df.phase.shift(7 * i - 1, freq="D")
+        df[f"lag_phase_{i}_week_after"] = df.phase.shift(7 * i + 1, freq="D")
+
+    return df.reset_index()
+
+
 def add_basic_valid_lag_features(df, n_lags_day, n_lags_month, n_lags_yr):
     df["date"] = pd.to_datetime(df["date"])
     df = df.set_index("date")
@@ -111,31 +131,12 @@ def add_basic_valid_lag_features(df, n_lags_day, n_lags_month, n_lags_yr):
             .fillna(df.phase.shift(365 + 30 * i - 1, freq="D"))
         )
 
-        # Como hacer el mean
-        # np.nanmean(
-        #     [
-        #         df.phase.shift(365 + 30 * i, freq="D"),
-        #         df.phase.shift(365 + 30 * i + 1, freq="D"),
-        #         df.phase.shift(365 + 30 * i - 1, freq="D"),
-        #     ],
-        #     axis=0,
-        # )
-
     for i in range(1, n_lags_yr + 1):
         df[f"lag_phase_{i}_yr"] = (
             df.phase.shift(365 + 365 * i, freq="D")
             .fillna(df.phase.shift(365 + 365 * i + 1, freq="D"))
             .fillna(df.phase.shift(365 + 365 * i - 1, freq="D"))
         )
-
-        # np.nanmean(
-        #     [
-        #         df.phase.shift(365 + 365 * i, freq="D"),
-        #         df.phase.shift(365 + 365 * i + 1, freq="D"),
-        #         df.phase.shift(365 + 365 * i - 1, freq="D"),
-        #     ],
-        #     axis=0,
-        # )
 
     return df.reset_index()
 
@@ -303,4 +304,14 @@ def get_week_inmonth(df: pd.DataFrame):
     df = df.merge(tmp, on=["year", "month"]).assign(
         num_week_month=lambda x: x.week - x.first_week
     )
+    return df
+
+
+def add_covid_exo(df, start_from = '2020-03-01', end_at = None):
+
+    df = df.copy()
+    df['covid'] = 0
+    df.loc[df.date >= start_from, 'covid'] = 1
+    if end_at is not None:
+        df.loc[df.date >= end_at, 'covid'] = 0
     return df
